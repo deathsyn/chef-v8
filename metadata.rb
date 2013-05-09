b3f9c3d5b515e4d480065931552596fa0f70b5fb
Kevin Berry <kevin@opensourcealchemist.com>
2013-05-09 07:28:02 -0500

    Explicit declaration of support for ubuntu 12.04.  #1

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,5 @@ license          "Apache 2.0"
 description      "Installs V8 javascript libraries"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+
+supports 'ubuntu', ">= 12.04"
